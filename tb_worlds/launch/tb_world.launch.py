506b22de9eae083bc8e83e12c2282db18f17991a
Aymon <aymonh2017@gmail.com>
2025-09-10 19:53:55 +0100

    update

--- tb_worlds/launch/tb_world.launch.py
+++ tb_worlds/launch/tb_world.launch.py
@@ -60,7 +60,7 @@ def generate_launch_description():
 
     declare_world_cmd = DeclareLaunchArgument(
         "world",
-        default_value=os.path.join(bringup_dir, "worlds", "sim_house.sdf.xacro"),
+        default_value=os.path.join(bringup_dir, "worlds", "warehouse.sdf.xacro"),
         description="Full path to world model file to load",
     )
 
@@ -123,18 +123,18 @@ def generate_launch_description():
     # running in headless mode. But currently, the Gazebo command line doesn't
     # take SDF strings for worlds, so the output of xacro needs to be saved into
     # a temporary file and passed to Gazebo.
-    world_sdf = tempfile.mktemp(prefix="tb_", suffix=".sdf")
-    world_sdf_xacro = ExecuteProcess(cmd=["xacro", "-o", world_sdf, world])
+    # world_sdf = tempfile.mktemp(prefix="tb_", suffix=".sdf")
+    # world_sdf_xacro = ExecuteProcess(cmd=["xacro", "-o", world_sdf, world])
     gazebo = ExecuteProcess(
-        cmd=["gz", "sim", "-r", world_sdf],
+        cmd=["gz", "sim", "-r", world],
         output="screen",
     )
 
-    remove_temp_sdf_file = RegisterEventHandler(
-        event_handler=OnShutdown(
-            on_shutdown=[OpaqueFunction(function=lambda _: os.remove(world_sdf))]
-        )
-    )
+    # remove_temp_sdf_file = RegisterEventHandler(
+    #     event_handler=OnShutdown(
+    #         on_shutdown=[OpaqueFunction(function=lambda _: os.remove(world_sdf))]
+    #     )
+    # )
 
     gz_tb_spawner = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
